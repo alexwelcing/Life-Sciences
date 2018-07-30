692a55229e5161694f0455eaa0c29938d8039353
Alex Welcing <alexwelcing@gmail.com>
2018-07-30 09:00:18 -0400

    730 Update

--- js/isotope.js
+++ js/isotope.js
@@ -4,7 +4,7 @@
  * Licensed GPLv3 for open source use
  * or Isotope Commercial License for commercial use
  *
- * https://isotope.metafizzy.co
+ * https://isotope.metafizzy.co 
  * Copyright 2010-2018 Metafizzy
  */
 
